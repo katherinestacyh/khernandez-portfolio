3d4519002ca7e967dc5e42e33690ab36f2b9562c
Katherine Hernandez <48598460+katherinestacyh@users.noreply.github.com>
2024-04-22 18:52:17 -0700

    changing to yarn

---
+++ src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-blue-500 text-white p-3">
+        <h1>Hello!</h1>
+        <p>
+          Welcome to my personal website, I'm Katherine
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
