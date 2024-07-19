6f3e9c7999997a27213b2551ca76df1b9502674c
Katherine Hernandez <48598460+katherinestacyh@users.noreply.github.com>
2024-07-18 22:26:16 -0700

    updates to colors

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,10 +4,10 @@ import me_nobackground from '../assets/me_nobackground.png';
 
 const AboutPage = () => {
   return (
-    <div className="h-screen flex flex-col">
+    <div className="h-screen flex flex-col bg-gray-900 text-white">
       <Navbar />
-      <div className="ml-10 flex-grow flex items-center justify-center font-mono">
-        <div className="text-pretty">
+      <div className="flex justify-center font-mono items-center">
+        <div className="text-pretty max-w-lg">
           <h1 className="text-2xl">Hi! I'm Katherine Hernandez.</h1>
           <h2 className="text-4xl mt-4 font-semibold">A Software Engineer.</h2>
           <h3 className="text-2xl mt-4">I enjoy a little bit of everything from Developing Applications and Websites to UI/UX Designing. My purpose is to help create great user experiences.</h3>
@@ -16,7 +16,7 @@ const AboutPage = () => {
         <img
           src={me_nobackground}
           alt="Katherine Hernandez Portrait"
-          className="png"
+          className="object-none"
         />
       </div>
       </div>
